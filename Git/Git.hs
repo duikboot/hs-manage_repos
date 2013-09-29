4b5f3721d1a845567506548337243ecb2a3340e3
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2013-09-29 15:35:54 +0200

    added new git thingie

--- Git/Git.hs
+++ Git/Git.hs
@@ -1,23 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
- 
 module Git where
  
-import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as T
-import Prelude hiding (FilePath)
-import Shelly
- 
-git :: [Text] -> Sh Text
-git = run "git"
- 
-git_ :: [Text] -> Sh ()
-git_ = run_ "git"
- 
-gitCommit_ :: [Text] -> Sh ()
-gitCommit_ = git_ . (:) "commit"
- 
-gitInit_ :: [Text] -> Sh ()
-gitInit_ = git_ . (:) "init"
- 
-gitAdd_ :: [Text] -> Sh ()
-gitAdd_ = git_ . (:) "add"
+import System.Process (createProcess, waitForProcess, shell)
+import System.Exit (ExitCode(..))
+import System.IO (hPutStrLn, stderr, stdout)
+
+
+git :: String -> IO ()
+git s = do
+    (_, _, _, ph) <- createProcess $ shell $ "git " ++ s
+    exit <- waitForProcess ph
+    case exit of
+        ExitSuccess -> do hPutStrLn stdout $ "cloned " ++ s
+        ExitFailure code -> do hPutStrLn stderr $ "git " ++ s ++ " script failed with exitcode: " ++ show code
+
+main ::  IO ()
+main = do
+    let temp = "clone http://github.com/duikboot/dorfiles"
+    git temp
