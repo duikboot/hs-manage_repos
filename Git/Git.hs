a79be1808c364d8bddbde8693c767d801943cadd
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2013-09-29 16:08:23 +0200

    working clone

--- Git/Git.hs
+++ Git/Git.hs
@@ -1,19 +1,34 @@
 module Git where
  
+import System.Environment (getArgs)
 import System.Process (createProcess, waitForProcess, shell)
 import System.Exit (ExitCode(..))
 import System.IO (hPutStrLn, stderr, stdout)
+import System.Directory (doesDirectoryExist) --, getHomeDirectory)
 
 
 git :: String -> IO ()
 git s = do
     (_, _, _, ph) <- createProcess $ shell $ "git " ++ s
+    print ("git " ++ s)
     exit <- waitForProcess ph
     case exit of
         ExitSuccess -> do hPutStrLn stdout $ "cloned " ++ s
         ExitFailure code -> do hPutStrLn stderr $ "git " ++ s ++ " script failed with exitcode: " ++ show code
 
+
+clone :: String -> String -> IO ()
+clone link target = do
+        exists <- doesDirectoryExist target
+        let s = link ++ " " ++ target
+        if (not exists)
+            then (git . (++) "clone ") s
+            else putStrLn "Directory exists"
+
+pull :: String -> IO ()
+pull = git . (++) "pull "
+
 main ::  IO ()
 main = do
-    let temp = "clone http://github.com/duikboot/dorfiles"
-    git temp
+    (link:path:_) <- getArgs
+    clone link path
